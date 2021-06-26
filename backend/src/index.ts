2e01500f3168b631bb85290066f4179bd9e85f5d
Sita Bérété <sita.berete.3@gmail.com>
2021-06-25 23:26:33 -0400

    feat: server (express)

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import app, { request, response } from "express"
+import { env } from "process"
+import { TokenManager } from "./TokenManager";
+
+const tokenManager = new TokenManager();
+
+app().get("/apitoken/:secret", async (request, response) => {
+
+  if(request.params.secret != env.CLIENT_ID as string + env.CLIENT_SECRET){
+    response.send(400) // BAD REQUEST
+  }
+
+  response.json({
+    token: tokenManager.getToken()
+  })
+
+}).listen(env.PORT || 8080);
