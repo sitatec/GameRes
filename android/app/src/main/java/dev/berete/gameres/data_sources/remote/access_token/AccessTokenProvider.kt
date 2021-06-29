474117b75ec7eec06d3fda6988a78638f63ca24d
Sita Bérété <sita.berete.3@gmail.com>
2021-06-29 16:57:08 -0400

    feat: fetch and cache access token    - fetch the access token from the backend server,      and cache it in the SharedPreferences.

--- android/app/src/main/java/dev/berete/gameres/data_sources/remote/access_token/AccessTokenProvider.kt
+++ android/app/src/main/java/dev/berete/gameres/data_sources/remote/access_token/AccessTokenProvider.kt
@@ -1,9 +1,43 @@
 package dev.berete.gameres.data_sources.remote.access_token
 
-import retrofit2.http.GET
+import android.content.SharedPreferences
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
- * The access token provider endpoints
+ * The access token provider with cache system
  */
-interface AccessTokenProviderClient {
+class AccessTokenProvider(
+    private val backendClient: BackendClient,
+    private val sharedPreferences: SharedPreferences,
+) {
+    private val accessTokenCacheKey = "API_ACCESS_TOKEN"
+
+    private var accessToken: String = sharedPreferences.getString(accessTokenCacheKey, "")!!
+
+    suspend fun getAccessToken(): String {
+        if (accessToken.isEmpty()) {
+            accessToken = fetchFromServerAndSave()
+        }
+        return accessToken
+    }
+
+    fun resetCache() {
+        sharedPreferences.edit().remove(accessTokenCacheKey).apply()
+    }
+
+    suspend fun fetchFromServerAndSave() = fetchTokenFromServer().also {
+        accessToken = it
+        sharedPreferences.edit().putString(accessTokenCacheKey, it).apply()
+    }
+
+
+    private suspend fun fetchTokenFromServer() = withContext(IO) {
+        backendClient.getAccessToken()["token"]!!
+    }
+
+
 }
--- android/app/src/main/java/dev/berete/gameres/data_sources/remote/access_token/BackendClient.kt
+++ android/app/src/main/java/dev/berete/gameres/data_sources/remote/access_token/BackendClient.kt
@@ -1,4 +1,28 @@
-package dev.berete.gameres.data_sources.remote
+package dev.berete.gameres.data_sources.remote.access_token
 
+import dev.berete.gameres.BuildConfig
+import retrofit2.http.GET
+
+/**
+ * The Backend endpoints
+ */
 interface BackendClient {
+    // We don't need to parse the response to a specific type because the returned json is very
+    // simple. i.e: { token: <the token here>}
+
+    /**
+     * Returns the access token in a Map with the key "token".
+     */
+    @GET("get")
+    suspend fun getAccessToken() : Map<String, String>
+
+    /**
+     * Make a request to refresh the access token and return the new one.
+     */
+    @GET("refresh")
+    suspend fun refreshToken() : Map<String, String>
+
+    companion object {
+        const val BACKEND_BASE_URL = BuildConfig.BACKEND_URL
+    }
 }
