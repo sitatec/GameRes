086c0e24080ba203f57f97a07168d90c111700a9
Sita Bérété <sita.berete.3@gmail.com>
2021-07-02 23:55:58 -0400

    fix: access token endpoint

--- android/app/src/main/java/dev/berete/gameres/data_sources/remote/access_token/BackendClient.kt
+++ android/app/src/main/java/dev/berete/gameres/data_sources/remote/access_token/BackendClient.kt
@@ -7,19 +7,18 @@ import retrofit2.http.GET
  * The Backend endpoints
  */
 interface BackendClient {
-    // We don't need to parse the response to a specific type because the returned json is very
-    // simple. i.e: { token: <the token here>}
+    // The returned json is very simple. i.e: { token: <the token here>}. So Map is sufficient.
 
     /**
      * Returns the access token in a Map with the key "token".
      */
-    @GET("get")
+    @GET("apitoken/${BuildConfig.BACKEND_ACCESS_KEY}/get")
     suspend fun getAccessToken() : Map<String, String>
 
     /**
      * Make a request to refresh the access token and return the new one.
      */
-    @GET("refresh")
+    @GET("apitoken/${BuildConfig.BACKEND_ACCESS_KEY}/refresh")
     suspend fun refreshToken() : Map<String, String>
 
     companion object {
